0d9e71b4942a45174d70a2058a4aee07b9daaf55
Muhammad Hafizh Azzasafah <85096618+Akanerd@users.noreply.github.com>
2022-06-24 16:26:27 +0700

    Upload bagian dragmasker
    Game bagian dragmasker

---
+++ Assets/Assets/Drag Masker/Script/Mask.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mask : MonoBehaviour
+{
+    private float RotationSpeed = 5;
+    public bool isDragging = false;
+
+    public void setRotationSpeed(float newRtSpeed)
+    {
+        RotationSpeed = newRtSpeed;
+    }
+    private void OnMouseDown()
+    {
+        isDragging = true;
+    }
+    private void OnMouseUp()
+    {
+        isDragging = false;
+    }
+
+    private void OnMouseDrag()
+    {
+        transform.Rotate((Input.GetAxis("Mouse Y") * RotationSpeed), 0, 0, Space.World);
+    }
+}
---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneController : MonoBehaviour
+{
+    public static SceneController Instance { get; set; }
+    int sceneToLoad=1;
+    int tempScene;
+    public Slider timer;
+    public float time;
+    public GameObject frame;
+    public GameObject endPanel;
+
+    private void Awake()
+    {
+        Time.timeScale = 1;
+        Instance = this;
+        sceneToLoad = SceneManager.GetActiveScene().buildIndex;
+        timer.maxValue = time;
+       
+    }
+    void Update()
+    {
+        timer.value += Time.deltaTime;
+        if (timer.value == timer.maxValue)
+        {
+            frame.SetActive(true);
+            
+        }
+        if (frame.activeSelf)
+        {
+            Time.timeScale = 0;
+            if (Input.GetMouseButtonDown(0))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+        }
+    }
+
+   
+    public void AnotherLevel()
+    {
+        endPanel.SetActive(true);
+        timer.value = 0;
+        //2 saniye sonra sahne değiştir
+        Invoke(nameof(toAnotherLevel), 2);
+        
+    }
+    public void toAnotherLevel()
+    {
+        //tempScene = Random.Range(1, 5);
+
+       // while (tempScene == sceneToLoad)
+       // {
+       //     tempScene = Random.Range(1, 5);
+       // }
+       // sceneToLoad = tempScene;
+       // SceneManager.LoadScene(sceneToLoad);
+        SceneManager.LoadScene("Dragmasker");
+    }
+
+}
